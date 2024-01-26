33e7e73d3773fbb0b08464bf794895b3f1dc73dd
loscarpab <ccormor392@g.educaand.es>
2024-01-26 20:22:18 +0100

    parte2 fin

--- app/src/main/java/com/ccormor392/coroutines/dosbotones/ui/DosBotonesViewModel.kt
+++ app/src/main/java/com/ccormor392/coroutines/dosbotones/ui/DosBotonesViewModel.kt
@@ -7,19 +7,32 @@ import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class DosBotonesViewModel:ViewModel() {
 
-    private var _resultState = MutableLiveData<Int>()
+    private var _resultState = MutableLiveData<Int>(0)
     var resulState : LiveData<Int> = _resultState
     private var _colorBoton = MutableLiveData<Boolean>(false)
     var colorBoton : LiveData<Boolean> = _colorBoton
 
-    fun bloquearApp(){
-        Thread.sleep(5000)
-        _resultState.value = _resultState.value?.plus(1)
+    fun bloquearApp() {
+        _resultState.value = resulState.value!!.plus(1)
+        //Nos permite crear una corrutina desde un ViewModel
+        viewModelScope.launch {
+            val result = withContext(Dispatchers.IO) {
+                delay(5000)
+                _resultState.value
+            }
+            _resultState.value = result!!
+        }
     }
+
     fun cambiarColor(){
         _colorBoton.value = !_colorBoton.value!!
     }
